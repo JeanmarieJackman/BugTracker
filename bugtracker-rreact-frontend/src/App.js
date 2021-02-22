68c5c952d8cdc6d273840e916c0ab06d2b736b2a
JeanmarieJackman <jeanmarie.jackman@gmail.com>
2021-02-22 17:26:47 -0600

    create states for the form fields in the constructor

--- bugtracker-rreact-frontend/src/App.js
+++ bugtracker-rreact-frontend/src/App.js
@@ -3,7 +3,27 @@ import { Button, Form, Container, Header } from 'semantic-ui-react'
 import './App.css';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: '',
+      age: '',
+      salary: '',
+      hobby: ''
+    }
+  }
+
+  changeHandler = (e) => {
+    this.setState({[e.target.name] : e.target.value})
+  }
+
+  submitHandler = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+  }
+
   render() {
+    const { name, age, salary, hobby } = this.state; (*)
     return (
       <Container fluid className="container">
         <Header as='h2'>React Google Sheets!</Header>
