c51112f8af884cf0eabd55e01d387f6321546872
JeanmarieJackman <jeanmarie.jackman@gmail.com>
2021-02-22 17:30:11 -0600

    update form fields to update state on submit

--- bugtracker-rreact-frontend/src/App.js
+++ bugtracker-rreact-frontend/src/App.js
@@ -23,26 +23,26 @@ export default class App extends Component {
   }
 
   render() {
-    const { name, age, salary, hobby } = this.state; (*)
+    const { name, age, salary, hobby } = this.state; 
     return (
       <Container fluid className="container">
         <Header as='h2'>React Google Sheets!</Header>
-        <Form className="form">
+        <Form className="form" onSubmit={this.submitHandler}>
           <Form.Field>
             <label>Name</label>
-            <input placeholder='Enter your name' />
+            <input placeholder='Enter your name' type="text" name = "name" value = {name} onChange={this.changeHandler}/>
           </Form.Field>
           <Form.Field>
             <label>Age</label>
-            <input placeholder='Enter your age' />
+            <input placeholder='Enter your age' type="number" name = "age" value = {age} onChange={this.changeHandler}/>
           </Form.Field>
           <Form.Field>
             <label>Salary</label>
-            <input placeholder='Enter your salary' />
+            <input placeholder='Enter your salary' type="number" name = "salary" value = {salary} onChange={this.changeHandler}/>
           </Form.Field>
           <Form.Field>
             <label>Hobby</label>
-            <input placeholder='Enter your hobby' />
+            <input placeholder='Enter your hobby' type="text" name = "hobby" value = {hobby} onChange={this.changeHandler}/>
           </Form.Field>
           
           <Button color="blue" type='submit'>Submit</Button>
