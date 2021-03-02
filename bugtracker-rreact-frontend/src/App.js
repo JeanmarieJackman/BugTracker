1025fb0713cfb6bd9a9394a1715367c35a6ceaaf
JeanmarieJackman <jeanmarie.jackman@gmail.com>
2021-03-01 23:58:31 -0600

    life stat up to App

--- bugtracker-rreact-frontend/src/App.js
+++ bugtracker-rreact-frontend/src/App.js
@@ -5,6 +5,27 @@ import BugReportForm from './components/BugReportForm'
 export default class App extends Component {
 
   render() {
+    state={
+      name: '',
+      age: '',
+      salary: '',
+      hobby: ''
+    }
+
+
+  changeHandler = (e) => {
+    this.setState({[e.target.name] : e.target.value})
+  }
+
+  submitHandler = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+
+    axios.post('https://sheet.best/api/sheets/54eb8b68-fcc3-4a30-9ed8-d1c23dff6fae', this.state)
+    .then(response => {
+      console.log(response)
+    })
+  }
     return (
       <BugReportForm/>
     )
--- bugtracker-rreact-frontend/src/components/BugReportForm.js
+++ bugtracker-rreact-frontend/src/components/BugReportForm.js
@@ -3,28 +3,8 @@ import axios from 'axios';
 import { Button, Form, Container, Header } from 'semantic-ui-react'
 
 export default class BugReportForm extends Component {
-  
-state={
-      name: '',
-      age: '',
-      salary: '',
-      hobby: ''
-    }
 
 
-  changeHandler = (e) => {
-    this.setState({[e.target.name] : e.target.value})
-  }
-
-  submitHandler = (e) => {
-    e.preventDefault();
-    console.log(this.state);
-
-    axios.post('https://sheet.best/api/sheets/54eb8b68-fcc3-4a30-9ed8-d1c23dff6fae', this.state)
-    .then(response => {
-      console.log(response)
-    })
-  }
 
   render() {
     const { name, age, salary, hobby } = this.state; 
